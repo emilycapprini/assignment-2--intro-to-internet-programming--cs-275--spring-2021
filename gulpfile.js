72083202b2bcc8130e0949a9806e3a03f9b02f60
Emily Capprini <ecapprini@gmail.com>
2021-04-15 11:05:56 -0400

    Add validateCSS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, series } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
+const cssValidator = require(`gulp-stylelint`);
 
 let validateHTML = () => {
     return src([
@@ -8,11 +9,19 @@ let validateHTML = () => {
         `html/**/*.html`])
         .pipe(htmlValidator());
 };
+
 let compressHTML = () => {
     return src([`html/*.html`,`html/**/*.html`])
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
 };
 
+let validateCSS = () => {
+    return src([`css/*.css`,`css/**/*.css`])
+        .pipe(cssValidator({reporters: [{formatter: `string`, console: true}]}))
+        .pipe(dest(`prod`));
+}
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
+exports.validateCSS = validateCSS;
