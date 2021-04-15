c3199f19a7a223360b6a93b247224cb25a80813c
Emily Capprini <ecapprini@gmail.com>
2021-04-15 10:27:49 -0400

    Add compressHTML task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const { src, dest, series } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
 let validateHTML = () => {
     return src([
-        `/html/*.html`,
-        `/html/**/*.html`])
+        `html/*.html`,
+        `html/**/*.html`])
         .pipe(htmlValidator());
+};
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
 };
 
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
