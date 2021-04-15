2ad86a6122792480cce94985af59bca555caa0d6
Emily Capprini <ecapprini@gmail.com>
2021-04-15 12:56:16 -0400

    Saving work

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest, series } = require(`gulp`);
+const { src, dest, series, watch } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const cssValidator = require(`gulp-stylelint`);
 const jsValidator = require(`gulp-eslint`);
 const babel = require(`gulp-babel`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
 
 let validateHTML = () => {
     return src([
@@ -36,8 +38,43 @@ let transpileJS = () => {
         .pipe(dest(`prod/js`));
 };
 
+
+let dev = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        server: {
+            baseDir: [
+                `./`,
+                `html`
+            ]
+        }
+    });
+
+    watch(`js/*.js`,
+        series(validateJS)
+    ).on(`change`, reload);
+
+    watch(`css/**/*.css`,
+        series(validateCSS)
+    ).on(`change`, reload);
+
+    watch(`html/**/*.html`,
+        series(validateHTML)
+    ).on(`change`, reload);
+
+    // watch(`dev/img/**/*`).on(`change`, reload);
+};
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.validateCSS = validateCSS;
 exports.validateJS = validateJS;
 exports.transpileJS = transpileJS;
+exports.dev = series(validateCSS, validateJS, validateHTML, dev);
+exports.build = series(
+    compressHTML,
+    // compileCSSForProd,
+    transpileJS,
+    // compressImages,
+);
